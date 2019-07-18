652e1a95da0f91ffd1f207fa58338c94ed68a879
Exequiel Mleziva <nathan_hyan@outlook.com>
2019-07-18 03:28:34 -0300

    Added mobile version

--- js/main.js
+++ js/main.js
@@ -1,23 +1,36 @@
-let navBar = document.querySelector("nav");
+let navBar = document.querySelector(".desktop");
 let mailInput = document.querySelector(".mailInput");
 let notify = document.querySelector(".notify");
 
 notify.addEventListener("click", function() {
   if (mailInput.value.length > 5) {
-    console.log("valido xd");
   } else {
-    console.log("a webo");
   }
 });
 
 window.onscroll = function() {
-  console.log(window.pageYOffset);
-
   if (window.pageYOffset < 500) {
     navBar.style.background = "transparent";
-    console.log("down");
   } else {
     navBar.style.background = "rgba(12, 15, 10, 0.6)";
-    console.log("up");
   }
 };
+
+let hamBtn = document.querySelector(".btn");
+let hamMenu = document.querySelector(".hamburgerMenu");
+let hidden = 1;
+
+hamBtn.addEventListener("click", function(e) {
+  e.preventDefault();
+
+  if (hidden === 1) {
+    console.log(hidden);
+    hamMenu.style.width = "0px";
+    hidden = 0;
+    console.log(hidden);
+  } else if (hidden === 0) {
+    hamMenu.style.width = "300px";
+    hidden = 1;
+    console.log(hidden);
+  }
+});
